9e643e2192e744dead92db2f3f4a245ac8863d77
George Sykes <gsykes537@gmail.com>
2026-08-22 11:06:26 +0000

    feat: restructure as a ros package

---
+++ src/core/trilobot-python/examples/print_distance.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import time
+
+from trilobot import BUTTON_A, Trilobot
+
+"""
+This demonstrates how to read distance values from Trilobot's ultrasound
+distance sensor. It will print the values it reads onto the console in cm,
+along with the time taken to get the readings.
+
+Stop the example by pressing button A.
+"""
+print("Trilobot Example: Print Distance\n")
+
+
+tbot = Trilobot()
+
+while not tbot.read_button(BUTTON_A):
+
+    # Take 10 measurements rapidly
+    for i in range(10):
+        clock_check = time.perf_counter()
+        distance = tbot.read_distance(timeout=25, samples=3)
+        print("Rapid:  Distance is {:.1f} cm (took {:.4f} sec)".format(distance, (time.perf_counter() - clock_check)))
+        time.sleep(0.01)
+
+    # Take 10 measurements allowing longer time for measuring greater distances
+    for i in range(10):
+        clock_check = time.perf_counter()
+        distance = tbot.read_distance(timeout=200, samples=9)
+        print("Slower: Distance is {:.1f} cm (took {:.4f} sec)".format(distance, (time.perf_counter() - clock_check)))
+        time.sleep(0.01)
