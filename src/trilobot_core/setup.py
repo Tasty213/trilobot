75f072939dee9396da976557a60444bfb78a16ac
George Sykes <gsykes537@gmail.com>
2026-08-24 17:47:13 +0000

    feat: update setup.py to reference the mock node

--- src/trilobot_core/setup.py
+++ src/trilobot_core/setup.py
@@ -1,32 +1,31 @@
 from setuptools import find_packages, setup
 
-package_name = 'trilobot_core'
+package_name = "trilobot_core"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
     ],
-    package_data={'': ['py.typed']},
-    install_requires=['setuptools', 'trilobot'],
+    package_data={"": ["py.typed"]},
+    install_requires=["setuptools", "trilobot"],
     zip_safe=True,
-    maintainer='George Sykes',
-    maintainer_email='gsykes537@gmail.com',
-    description='TODO: Package description',
-    license='MIT',
+    maintainer="George Sykes",
+    maintainer_email="gsykes537@gmail.com",
+    description="TODO: Package description",
+    license="MIT",
     extras_require={
-        'test': [
-            'pytest',
+        "test": [
+            "pytest",
         ],
     },
     entry_points={
-        'console_scripts': [
-            'driver_node = trilobot_core.driver_node:main',
-            'hello_node = trilobot_core.hello_node:main'
+        "console_scripts": [
+            "driver_node = trilobot_core.driver_node:main",
+            "mock_node = trilobot_core.mock_node:main",
         ],
     },
 )
