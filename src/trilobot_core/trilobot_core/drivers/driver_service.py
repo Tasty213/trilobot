267d95071e060b1e74576bb4a120a5fb9a5670e3
George Sykes <gsykes537@gmail.com>
2026-08-26 16:46:43 +0000

    feat: add button state publishing

--- src/trilobot_core/trilobot_core/drivers/driver_service.py
+++ src/trilobot_core/trilobot_core/drivers/driver_service.py
@@ -2,11 +2,16 @@ import trilobot_core.drivers.buttons as Buttons
 import trilobot_core.drivers.lights as Lights
 import trilobot_core.drivers.motors as Motors
 from rclpy.node import Node
+from std_msgs.msg import Float32
 from trilobot import Trilobot
 from trilobot_core.drivers.abstract_driver import StaticRobotService
+from trilobot_interfaces.srv import GetButtonsState
 
 
 import inspect
+
+DISTANCE_PUBLISH_PERIOD = 0.1
+BUTTON_STATES_PUBLISH_PERIOD = 0.1
 
 
 class DriverService(Node):
@@ -72,16 +77,26 @@ class DriverService(Node):
             )
             self.light_services[driver_name] = new_service
 
+        self.distance_publisher = self.create_publisher(Float32, "distance", 10)
+        self.distance_timer = self.create_timer(
+            DISTANCE_PUBLISH_PERIOD, self.publish_distance
+        )
+
+        self.button_states_publisher = self.create_publisher(
+            GetButtonsState.Response, "button_states", 10
+        )
+        self.button_states_timer = self.create_timer(
+            BUTTON_STATES_PUBLISH_PERIOD, self.publish_button_states
+        )
+
         self.logger.info(("Trilobot driver ready"))
 
-    def read_distance():
-        pass
+    def publish_distance(self):
+        msg = Float32()
+        msg.data = self.tbot.read_distance()
+        self.distance_publisher.publish(msg)
 
-    def get_buttons_state():
-        pass
-
-    def set_button_led():
-        pass
-
-    def set_button_led_brightness():
-        pass
+    def publish_button_states(self):
+        msg = GetButtonsState.Response()
+        msg.buttons = [self.tbot.read_button(i) for i in range(4)]
+        self.button_states_publisher.publish(msg)
